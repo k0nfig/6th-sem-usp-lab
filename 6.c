f8f15b29aa8ff0927e44eedbefbcd43f3457519e
Sameera S <sameera1595@gmail.com>
2015-05-20 13:14:16 +0530

    usp programs

--- 6.c
+++ 6.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
 
+//To display each character at a time! To show both parent and child have conflicting behaviours
 void chardisp(char *ptr)
 {
 	char *str=ptr;
 	int w;
 	for(;w=*str;str++)
 	{
-		putc(w,stderr);
+		putc(w,stderr);// putc(int,FILE &);
 	}
 }
 
@@ -21,7 +22,7 @@ void main()
 	}
 	else if(pid==0)
 	{
-		chardisp("I am child\n");
+		chardisp("I am child\n");//Fork does not guarntee who executes first. whether parent or child
 	}
 	else
 	{
@@ -29,3 +30,4 @@ void main()
 	}
 }
 // For best results : Always compile before pressing ./a.out everytime
+//Compile each and everytime before executing to get different answers!
