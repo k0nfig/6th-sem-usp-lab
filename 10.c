f0b62b7f0c244b4977676de79967742b30086a83
Sameera S <sameera1595@gmail.com>
2015-05-20 13:17:57 +0530

    usp lab

--- 10.c
+++ 10.c
@@ -3,6 +3,7 @@
 #include<signal.h>
 #define INTERVAL 5
 
+//callme is a function pointer. It is called when SIGALRM is encountered by the process
 void callme(int signum)
 {
 	printf("Time up!\n");
@@ -10,7 +11,7 @@ void callme(int signum)
 
 void main()
 {
-	signal(SIGALRM,callme);
-	alarm(INTERVAL);
-	pause();
+	signal(SIGALRM,callme);//Setting up the signal handler for SIGALRM signal
+	alarm(INTERVAL);//calling the alarm function to raise a SIGALRM signal
+	pause();//Indefinite wait until a signal is caught
 }
