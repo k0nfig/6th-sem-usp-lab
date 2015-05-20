d42fa7d128c711ad861d2ceff9546e76df84cc9d
Sameera S <sameera1595@gmail.com>
2015-05-20 13:02:33 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -8,45 +8,50 @@ void main(int argc,char **argv)
 	int fd;
 	char buf[150];
 	struct flock f;
+        //Provide 2 arguments. ./a.out and the filename
 	if(argc!=2)
 	{
 		perror("Input error");
 		return;
 	}
+        //open the file for both read and write!
 	fd=open(argv[1],O_RDWR);
 	printf("Press enter to lock\n");
 	getchar();
-	f.l_type=F_WRLCK;
-	f.l_whence=SEEK_SET;
-	f.l_start=0;
-	f.l_len=100;
-	if((fcntl(fd,F_SETLK,&f))==-1)
+        //Defining all the locking conditions
+	f.l_type=F_WRLCK; //Type of lock : Write lock
+	f.l_whence=SEEK_SET; //Cursor pointing : Beginning of the file
+	f.l_start=0;//Start : from the beginning of the file(i.e 0)
+	f.l_len=100;//Length of lock: Till the 100th character from start
+	if((fcntl(fd,F_SETLK,&f))==-1) // SETLOCK : If the lock is being set by other process then it returns -1
 	{
-		fcntl(fd,F_GETLK,&f);
-		printf("Pid is %d\n",f.l_pid);
+		fcntl(fd,F_GETLK,&f); //F_GETLK=Get the details of the process which has locked the file and store it in f
+		printf("Pid is %d\n",f.l_pid);//Printing pid of the process which has locked it!
 	}
 	printf("Locked\n");
-	if(lseek(fd,SEEK_END-50,SEEK_END)==-1)
+	if(lseek(fd,SEEK_END-50,SEEK_END)==-1)//Cursor should point in the last 50th position
 	{
 		perror("Lseek error");
 		exit(0);
 	}
-	if(read(fd,buf,50)==-1)
+	if(read(fd,buf,50)==-1)//Reading 50 characters from the file and storing it in buf
 	{
 		perror("read error");
 		exit(0);
 	}
-	puts(buf);
-	f.l_type=F_UNLCK;
-	f.l_whence=SEEK_SET;
-	f.l_start=0;
-	f.l_len=0;
-	if((fcntl(fd,F_UNLCK,&f))==-1)
+	puts(buf);//Prints it onto the standard output
+	f.l_type=F_UNLCK;//Type of lock : unlock
+	f.l_whence=SEEK_SET;//Cursor position : from the beginning
+	f.l_start=0;//Start:0th position(starting of the file)
+	f.l_len=0;//Length : 0 from the starting position
+        //Read the above statement like this: It allows 0 positions to set the lock. i.e unlock the whole file!
+        //if start=0 and len=0 then the lock is applied on the whole file!
+	if((fcntl(fd,F_UNLCK,&f))==-1)//Setting up the lock
 	{
 		perror("fcntl error");
 		return;
 	}
 	printf("Unlocked\n");
-	close(fd);
+	close(fd);//Closing the file!
 	return;
 }
