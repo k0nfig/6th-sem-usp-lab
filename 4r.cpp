049e1443b7b196fd1edf950f1629109f45f86e3f
Sameera S <sameera1595@gmail.com>
2015-06-01 18:15:35 +0530

    usp lab

--- 4r.cpp
+++ 4r.cpp
@@ -6,15 +6,19 @@ using namespace std;
 
 int main(int argc,char **argv)
 {
-	int fd,res=0;
+	int fd;
 	char buf[50];
 	if(argc!=2)
 	{
 		cout<<"Input error\n";
 		return 0;
 	}
-	fd=open(argv[1],O_RDONLY);
-	res=read(fd,buf,50);
+	fd=open(argv[1],O_RDONLY); //Opening the same common file given as an argument in both terminals
+	cout<<"Waiting for data to be entered in the write terminal\n";
+	while(read(fd,buf,50)==-1)//read function returns -1 if the file is empty
+	{
+		;//making the terminal wait until data is entered
+	}
 	puts(buf);
 	cout<<"Data read!\n";
 	close(fd);
