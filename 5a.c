d098879f3f47dffbfe5bcb12a208aae3b10747b1
Sameera S <sameera1595@gmail.com>
2015-06-04 20:52:31 +0530

    usp lab

--- 5a.c
+++ 5a.c
@@ -3,11 +3,16 @@
 
 void main()
 {
+	//Okay, if you have to print a specific environment variable then uncomment all the below line
 	extern char **environ;
-	char **p;
+	char **p,*d,*cmd="SHELL";//Give the environment variable whichever you want to print for ex. SHELL 
 	printf("ENVIRONMENT VARIABLES\n");
 	for(p=environ;*p!='\0';p++)
 	{
-		printf("%s\n",*p);
+		//d=strstr(*p,cmd);
+		//if(d)
+		//{
+			printf("%s\n",*p);
+		//}
 	}
 }
